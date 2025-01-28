c372081e605c751621898c6828f70bbcda759528
allanDev9 <allanpenaguerra2003@gmail.com>
2025-01-27 19:03:54 -0600

    movies json

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 import { PeliculasJSON } from "./componets/data/PeliculasData"
-import { Peliculas } from "./componets/Peliculas"
+import { Peliculas } from "./componets/page/Peliculas"
 
 export const App = (): JSX.Element => {
   const [peliculas] = useState(PeliculasJSON)
 
   return (
     <>
-      <div className="bg-black h-screen">
+      <div className="bg-black h-auto">
         <Peliculas peliculas={peliculas} />
       </div>
     </>
--- src/componets/page/Peliculas.tsx
+++ src/componets/page/Peliculas.tsx
@@ -3,7 +3,9 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import { ListofMovies } from './types.d';
+import { ListofMovies } from '../types.d';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 interface Props {
     peliculas: ListofMovies[];
@@ -13,18 +15,19 @@ export const Peliculas: React.FC<Props> = ({ peliculas }) => {
     return (
         <>
             <h1 className='text-white text-center text-6xl mb-10 pt-10 font-bold'>Películas</h1>
-            <div className="flex flex-row gap-10 justify-center flex-wrap">
+
+            <div className="flex overflow-x-auto bg-blue-700 m-10 gap-10 justify-start p-4" style={{ maxHeight: '70vh' }}>
                 {peliculas.map((pelicula) => (
-                    <Card className='w-[20%] h-[55vh]' key={pelicula.id}>
+                    <Card className='w-[15%] flex-shrink-0 h-[55vh] m-5' key={pelicula.id}>
                         <CardContent>
                             <div className='m-[-18px]'>
                                 <img
-                                    className='w-[100%] h-[30vh]'
+                                    className='w-[100%] h-[30vh] object-cover'
                                     src={pelicula.image}
                                     alt={pelicula.titulo}
                                 />
-                                <div className='text-center mt-[-8] h-[17vh] bg-blue-200'>
-                                    <p className='text-3xl font-bold mt-[-15] mb-5'>{pelicula.titulo}</p>
+                                <div className='text-center mt-[-8px] h-[17vh] bg-blue-200'>
+                                    <p className='text-3xl font-bold mt-2 mb-5'>{pelicula.titulo}</p>
                                     <p className='font-bold text-red-500'>{pelicula.duracion}</p>
                                     <p className='font-bold text-red-500'>Categoria: {pelicula.categoria}</p>
                                 </div>
@@ -36,6 +39,17 @@ export const Peliculas: React.FC<Props> = ({ peliculas }) => {
                     </Card>
                 ))}
             </div>
+            <div className='flex justify-center'>
+                <div className="card bg-white w-[60%] text-black mb-10">
+                    <DataTable value={peliculas} tableStyle={{ minWidth: '50rem' }}
+                        paginator rows={5} rowsPerPageOptions={[5, 10, 15]}
+                    >
+                        <Column field="titulo" header="Titulo"></Column>
+                        <Column field="categoria" header="Categoria"></Column>
+                        <Column field="duracion" header="Duración"></Column>
+                    </DataTable >
+                </div >
+            </div >
         </>
     );
 };
