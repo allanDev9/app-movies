0f5f72db6f6448ab8029e497eba528e7bd2c1098
allanDev9 <allanpenaguerra2003@gmail.com>
2025-02-06 12:04:57 -0600

    responsive design

--- src/componets/page/Peliculas.jsx
+++ src/componets/page/Peliculas.jsx
@@ -15,19 +15,19 @@ import { appMoviesApi } from '../../services/appMoviesApi';
 
 export const Peliculas = ({ peliculas, username }) => {
     const navigate = useNavigate();
-    const [listEmpleados, setListEmpleados] = useState([]);
+    const [listUsuarios, setListUsuarios] = useState([]);
 
     useEffect(() => {
-        const fetchEmpleados = async () => {
+        const fetchUsurios = async () => {
             try {
-                const response = await appMoviesApi.get('empleados');
-                setListEmpleados(response.data);
+                const response = await appMoviesApi.get('usuarios');
+                setListUsuarios(response.data);
             } catch (error) {
-                console.error('Error fetching empleados:', error);
+                console.error('Error fetching usuarios:', error);
             }
         };
 
-        fetchEmpleados();
+        fetchUsurios();
     }, []);
 
     const handleBack = () => {
@@ -37,19 +37,11 @@ export const Peliculas = ({ peliculas, username }) => {
     return (
         <>
             <div className='bg-black h-auto pb-5'>
-                <h1 className='text-white font-serif text-center text-6xl mb-10 pt-10 font-bold max-sm:text-start max-sm: ml-10 max-sm:mb-[-10px] max-md:text-start max-md:mb-[-10px] max-lg:text-start max-lg:mb-[-10px]'>Películas</h1>
+                <h1 className='text-white font-serif text-center text-6xl mb-10 pt-10 font-bold max-sm:text-start max-sm:ml-7 max-sm:text-4xl max-sm:mb-[-5px] max-md:text-start max-md:mb-[-10px] max-lg:text-start max-lg:mb-[-10px]'>Películas</h1>
                 <div className='flex justify-end mr-10 flex-row'>
-                    <h1 className='text-blue-700 mr-10 mt-[-6%] font-bold flex-wrap text-2xl max-sm:mr-[10px] max-md:mr-[10px] max-md:mt-[-40px] max max-lg:mr-[10px] max-xl:mt-[-40px] '><AccountCircleIcon fontSize='large' /> {username}</h1>
-                    <Button
-                        variant='outlined'
-                        color='error'
-                        style={{ position: 'relative', right: '10px', top: '-90px' }}
-                        onClick={handleBack}
-                    >
-                        Cerra Sesión
-                    </Button>
+                    <h1 className='text-yellow-400 mr-10 mt-[-6%] font-bold flex-wrap text-2xl max-sm:mr-[-30px] max-sm:mt-[-35px]  max-md:mr-[10px] max-md:mt-[-40px] max max-lg:mr-[10px] max-xl:mt-[-40px] '><AccountCircleIcon fontSize='large' /> {username}</h1>
                 </div>
-                <div className="flex overflow-x-auto bg-blue-200 m-10 gap-10 rounded-xl justify-start p-4 max-sm:flex-col max-sm:w-[85%] max-md:w-[89%]" style={{ maxHeight: '70vh' }}>
+                <div className="flex overflow-x-auto bg-blue-200 m-10 gap-10 rounded-xl justify-start p-4 max-sm:flex-col max-sm:w-[65%] max-md:w-[89%]" style={{ maxHeight: '70vh' }}>
                     {peliculas.map((pelicula) => (
                         <Card className='diseño-peliculas w-[15%] flex-shrink-0 h-[55vh] m-5 max-sm:w-[90%] max-md:w-[50%] max-lg:w-[40%] max-xl:w-[35%]' key={pelicula.id}>
                             <CardContent>
@@ -76,15 +68,15 @@ export const Peliculas = ({ peliculas, username }) => {
             <div className='flex justify-center bg-red-700 pt-10'>
                 <div className="card bg-white w-[60%] text-black mb-10">
                     <h1 className='text-center text-4xl font-bold text-red-700 mb-5'>Peliculas disponibles</h1>
-                    <DataTable className='text-xl ml-5 mb-5' value={peliculas}
+                    <DataTable className='text-xl ml-5 mb-5' value={listUsuarios}
                         showGridlines
                         stripedRows
                         size='small'
                         paginator rows={5} rowsPerPageOptions={[5, 10, 15]}
                     >
-                        <Column field="titulo" header="Titulo"></Column>
-                        <Column field="categoria" header="Categoria"></Column>
-                        <Column field="duracion" header="Duración"></Column>
+                        <Column field="nombre" header="Titulo"></Column>
+                        <Column field="usuario" header="Categoria"></Column>
+                        <Column field="password" header="Duración"></Column>
                     </DataTable >
                 </div >
             </div >
